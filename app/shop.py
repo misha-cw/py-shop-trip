c3a6cb6d6cf90d61a2eb3282e06ba8f7af58d2d0
misha-cw <bigun.mykhaylo@gmail.com>
2025-02-05 18:20:48 +0200

    solution

--- app/shop.py
+++ app/shop.py
@@ -28,14 +28,14 @@ class Shop:
     def purchase_receipt(self, customer: Customer) -> None:
         shopping_list = [
             f"{customer.product_cart[product]} {product}s for "
-            f"{
+            f"""{
                 customer.product_cart[product] * self.products[product]
                 if customer.product_cart[product]
                 * self.products[product] % 1 != 0
                 else int(
                     customer.product_cart[product] * self.products[product]
                 )
-            } dollars\n"
+            } dollars\n"""
             for product in customer.product_cart
             if product in self.products
         ]
